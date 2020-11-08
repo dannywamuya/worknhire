023f1b08c75eba97e33cbe5cf049da05a383cb5a
Danny Wamuya <dannywamuya@gmail.com>
2020-11-08 22:34:23 +0300

    Fix worker detail

--- src/app/components/worker-detail/worker-detail.component.ts
+++ src/app/components/worker-detail/worker-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Worker } from 'src/app/models/worker';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-worker-detail',
@@ -12,12 +13,17 @@ export class WorkerDetailComponent implements OnInit {
 
   worker: Worker;
 
-  constructor(private dataService: DataService, private route: ActivatedRoute) { }
+  constructor(private dataService: DataService, private route: ActivatedRoute, private http:HttpClient) { }
 
   ngOnInit() {
   let id = parseInt(this.route.snapshot.paramMap.get('id'));
-  return this.dataService.getWorker(id)
-    // .subscribe(data => this.worker = data);
+  // return this.dataService.getWorker(id)
+  //   // .subscribe(data => this.worker = data);
+  // }
+  
+  return this.http.get<any>('https://worknhireapi.herokuapp.com/workers/' + id)
+    .subscribe(data => {
+        this.worker = data;
+    })
   }
-
 }
